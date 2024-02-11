b2df9f13230af023e21774d8af2330037182c19c
Anis Sahed <sahedanis2018@gmail.com>
2024-02-11 03:21:42 +0100

    feat : adding initial modal to create a server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,8 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from 'next/navigation';
+import { InitialModal } from "@/components/modals/initial-modal";
+
 const SetupPage = async () => {
     const profile = await initialProfile();
 
@@ -18,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
     }
     
-    return <div>Create a Server</div>;
+    return <InitialModal />;
 }
  
 export default SetupPage;
