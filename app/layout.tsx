c0844ee7a457d6a862737408a52efa380127182c
Anis Sahed <sahedanis2018@gmail.com>
2024-02-11 02:54:23 +0100

    feat : adding prisma + defining models

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
+            forcedTheme='dark'
             enableSystem={false}
             storageKey="discord-theme">
             {children}
