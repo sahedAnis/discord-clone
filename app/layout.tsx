aa43306aaaa79a88f3640e537c595e230eb0a8f9
Anis Sahed <sahedanis2018@gmail.com>
2024-02-11 16:03:57 +0100

    feat : server creation API + adding navigation side bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            forcedTheme='dark'
             enableSystem={false}
             storageKey="discord-theme">
             {children}
